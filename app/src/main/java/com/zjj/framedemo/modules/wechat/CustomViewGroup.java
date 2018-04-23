ba0f650a7ec708cb62dd2d8d3853e4e9d061a8d5
zhoujunjie <zhoujunjie@jubao56.com>
2018-04-23 17:45:54 +0800

    自定义viewGroup(一)

--- app/src/main/java/com/zjj/framedemo/modules/wechat/CustomViewGroup.java
+++ app/src/main/java/com/zjj/framedemo/modules/wechat/CustomViewGroup.java
@@ -2,11 +2,12 @@ package com.zjj.framedemo.modules.wechat;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.View;
 import android.view.ViewGroup;
 
 /**
  * Created by zhoujunjie on 2018/4/18.
- * 1. 继承viewgroup 必须重写Onlayout方法(不用像继承view去实现ondraw的方法)
+ * 1. 继承viewgroup 必须重写Onlayout方法
  * 2. 实现构造方法
  * 3. 自定义控件中两个重要的方法 onMeasure
  */
@@ -26,6 +27,52 @@ public class CustomViewGroup extends ViewGroup {
     }
 
     /**
+     * 重写onMeasure 方法
+     * 绘制viewgroup的宽度和高度
+     * @param widthMeasureSpec
+     * @param heightMeasureSpec
+     */
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        // 获取子控件的宽高模式
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+
+        // 获取子控件的宽高大小
+        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+
+        int count = getChildCount();
+        if (count == 0) {   // 没有子控件时
+            setMeasuredDimension(0, 0);
+        }
+        View child;
+        MarginLayoutParams layoutParams;
+        int width = 0;
+        int height = 0;
+        for (int i = 0 ; i < count;i++) {
+            child = getChildAt(i);
+            // 获取子控件的其它属性(外边距)
+            layoutParams = (MarginLayoutParams) child.getLayoutParams();
+            if (widthMode == MeasureSpec.AT_MOST && heightMode == MeasureSpec.AT_MOST) {
+                // 子控件的宽高都是wrap_content的时候
+                width += child.getMeasuredWidth()+layoutParams.leftMargin+layoutParams.rightMargin;
+                height += child.getMeasuredHeight()+layoutParams.topMargin+layoutParams.bottomMargin;
+                setMeasuredDimension(width, height * getChildCount());
+            } else if (widthMode == MeasureSpec.AT_MOST) {
+                width += child.getMeasuredWidth()+layoutParams.leftMargin+layoutParams.rightMargin;
+                setMeasuredDimension(width,heightSize);
+            } else if (heightMode == MeasureSpec.AT_MOST) {
+                height += child.getMeasuredHeight()+layoutParams.topMargin+layoutParams.bottomMargin;
+                setMeasuredDimension(widthSize,height);
+            }
+        }
+        measureChildren(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    /**
      * @param changed  给子控件布局
      * @param l       left 左边
      * @param t       top 顶部
@@ -35,10 +82,30 @@ public class CustomViewGroup extends ViewGroup {
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
         int height = 0;
-        // 获取子控件的总高度
+        View child;
         for (int i = 0; i < getChildCount();i++) {
-            int childHeight = getChildAt(i).getMeasuredHeight();
-            height += childHeight;
+            // 获取每个子控件
+            child = getChildAt(i);
+            // 放置每个子控件的位置
+            child.layout(0,height,child.getMeasuredWidth(),height+child.getMeasuredHeight());
+            height += child.getMeasuredHeight();
         }
+    }
+
+    /** 覆写下列方法 以求返回MarginLayoutParams*/
+    @Override
+    public LayoutParams generateLayoutParams(AttributeSet attrs) {
+        return new MarginLayoutParams(getContext(),attrs);
+    }
+
+    @Override
+    protected LayoutParams generateDefaultLayoutParams() {
+        return new MarginLayoutParams(LayoutParams.MATCH_PARENT,
+                LayoutParams.MATCH_PARENT);
+    }
+
+    @Override
+    protected LayoutParams generateLayoutParams(LayoutParams p) {
+        return new MarginLayoutParams(p);
     }
 }
